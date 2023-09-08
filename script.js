fae23f599f33a9d96b368813ae12178c99eff0a0
Nilushiya <nilushiyak@gmail.com>
2023-09-08 11:08:30 +0530

    border color

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const imgs = document.querySelectorAll(".img a");
+console.log(imgs)
 let imgId = 1;
+
+const imgdiv = document.querySelectorAll(".img");
 
 imgs.forEach((img)=>{
     img.addEventListener('click',(e)=>{
         e.preventDefault();
+        img.parentElement.classList.add("active")//parentElement
         imgId = img.dataset.id;
         console.log(imgId)
         moveImage();
     })
 })
+
+imgdiv.forEach((imgg) => {
+  imgg.classList.remove("active")
+})
+
+
 function moveImage()
 {
   const imgWidth=document.querySelector('.main-image img:first-child').clientWidth;
