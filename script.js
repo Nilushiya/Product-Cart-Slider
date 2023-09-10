e9805115004661ee971c7ef86fd120a86df37d2c
Nilushiya <nilushiyak@gmail.com>
2023-09-10 09:00:49 +0530

    star rate

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const imgs = document.querySelectorAll(".img a");
-console.log(imgs)
+//console.log(imgs)
 let imgId = 1;
 
 const imgdiv = document.querySelectorAll(".img");
+const stars = document.querySelectorAll(".star1");
+const  para= document.querySelector("#para");
+//console.log(para);
 
 imgs.forEach((img)=>{
     img.addEventListener('click',(e)=>{
         e.preventDefault();
+        imgdiv.forEach((imgg) => {
+          imgg.classList.remove("active")
+        })
         img.parentElement.classList.add("active")//parentElement
         imgId = img.dataset.id;
-        console.log(imgId)
+      //  console.log(imgId)
         moveImage();
     })
 })
-
-imgdiv.forEach((imgg) => {
-  imgg.classList.remove("active")
-})
-
 
 function moveImage()
 {
@@ -25,5 +26,8 @@ function moveImage()
   let width=((imgId-1)*imgWidth);
  
 document.querySelector('.main-image').style.transform=`translateX(${-width}px)`;
-
 }
+console.log(stars)
+
+let int = stars.indexOf("button.star1")
+console.log(int);
